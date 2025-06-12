2147d309e994bfc9f3148cf35ed8fa90a60855e1
afowdar <anjanifowdar@gmail.com>
2025-06-12 16:54:34 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "Topic :: Scientific/Engineering :: Chemistry",
-        "Topic :: Scientific/Engineering :: Bioinformatics",
+        "Topic :: Scientific/Engineering :: Bio-informatics",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
